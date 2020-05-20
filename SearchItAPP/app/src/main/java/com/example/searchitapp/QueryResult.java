90eb12b6206ef2ef1c96b9cdf6f6f125a87ea50c
HossamAh <hossamahmed201515@gmail.com>
2020-05-20 05:55:27 +0200

    new format

---
+++ SearchItAPP/app/src/main/java/com/example/searchitapp/QueryResult.java
@@ -0,0 +1,128 @@
+package com.example.searchitapp;
+
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.fragment.app.FragmentTransaction;
+import com.example.searchitapp.models.QueryResultItem;
+
+
+import java.util.ArrayList;
+
+public class QueryResult extends AppCompatActivity{
+    private ArrayList<QueryResultItem> ResultsList,CurrentResultPage;
+    private TextView ResultsNumber;
+    private TextView CurrentPage;
+    private ImageButton NextPage;
+    private ImageButton PreviousPage;
+    private ResultsPageFragment pagesFragment;
+    private FragmentTransaction ft;
+    private int CurrentPageNumber =1;
+    private int TotalPagesNumber;
+    private int previousLimit;
+    private ArrayList<String> QueryKeys;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_query_result2);
+        ResultsNumber = (TextView)findViewById(R.id.result_number_textView);
+        CurrentPage = (TextView)findViewById(R.id.CurrentPAge_textView);
+        NextPage =(ImageButton)findViewById(R.id.Next_Button);
+        PreviousPage = (ImageButton)findViewById(R.id.Previous_Button);
+
+        ResultsList =new ArrayList<>();
+        //ResultsList = getIntent().getParcelableArrayListExtra("Results");
+        QueryKeys =new ArrayList<>();
+        //QueryKeys = getIntent().getStringArrayListExtra("QueryKeys");
+        QueryKeys.add("facebook");
+        QueryKeys.add("twitter");
+        QueryKeys.add("instagram");
+
+
+        QueryResultItem ResultItem1 =new QueryResultItem(0,"Facebook|Login or sign up",
+                "https://www.facebook.com/","facebook about",null,null);
+        QueryResultItem ResultItem2 =new QueryResultItem(0,"Twitter|Login or sign up",
+                "https://www.twitter.com/","twitter about",null,null);
+        QueryResultItem ResultItem3 =new QueryResultItem(0,"Instagram|Login or sign up",
+                "https://www.Instagram.com/","instagram about",null,null);
+        for(int i=0;i<10;i++)
+            ResultsList.add(ResultItem1);
+        for(int i=0;i<10;i++)
+            ResultsList.add(ResultItem2);
+        for(int i=0;i<10;i++)
+            ResultsList.add(ResultItem3);
+
+
+
+        TotalPagesNumber= (int) Math.ceil((ResultsList.size()/10.0));
+
+        ResultsNumber.setText("results contain "+ResultsList.size()+" pages");
+        CurrentPage.setText(CurrentPageNumber+"/"+TotalPagesNumber);
+        CurrentResultPage = new ArrayList<>();
+        CurrentResultPage.addAll(ResultsList.subList(0,CurrentPageNumber*10));
+
+        pagesFragment = ResultsPageFragment.newInstance(CurrentResultPage,QueryKeys);
+        ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.ResultPage_FrameLayout, pagesFragment);
+        ft.commit();
+        if(CurrentPageNumber ==1 )
+        {
+            PreviousPage.setClickable(false);
+        }
+
+        PreviousPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(CurrentPageNumber > 1) {
+                    CurrentPageNumber--;
+                    if(CurrentPageNumber<TotalPagesNumber)
+                    {
+                        NextPage.setClickable(true);
+                    }
+                    if(CurrentPageNumber>0)
+                        previousLimit = CurrentPageNumber-1;
+                    CurrentPage.setText(CurrentPageNumber+"/"+TotalPagesNumber);
+                    CurrentResultPage = new ArrayList<>();
+                    CurrentResultPage.addAll(ResultsList.subList(previousLimit*10, CurrentPageNumber * 10));
+                    pagesFragment = ResultsPageFragment.newInstance(CurrentResultPage,QueryKeys);
+                    ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.ResultPage_FrameLayout, pagesFragment).addToBackStack(null);
+                    ft.commit();
+
+                    if (CurrentPageNumber == 1) {
+                        PreviousPage.setClickable(false);
+                    }
+                }
+            }
+        });
+        NextPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(CurrentPageNumber<= TotalPagesNumber) {
+                    CurrentPageNumber += 1;
+                    if(CurrentPageNumber>1)
+                    {
+                        PreviousPage.setClickable(true);
+                    }
+                    if(CurrentPageNumber>0)
+                        previousLimit = CurrentPageNumber-1;
+                    CurrentPage.setText(CurrentPageNumber+"/"+TotalPagesNumber);
+                    CurrentResultPage = new ArrayList<>();
+                    CurrentResultPage.addAll(ResultsList.subList(previousLimit*10,CurrentPageNumber*10));
+                    pagesFragment = ResultsPageFragment.newInstance(CurrentResultPage,QueryKeys);
+                    ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.ResultPage_FrameLayout, pagesFragment).addToBackStack(null);
+                    ft.commit();
+
+                    if(CurrentPageNumber==TotalPagesNumber)
+                    {
+                        NextPage.setClickable(false);
+                    }
+                }
+            }
+        });
+    }
+
+}
---
+++ SearchItAPP/app/src/main/java/com/example/searchitapp/ResultsPageFragment.java
@@ -0,0 +1,78 @@
+package com.example.searchitapp;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.searchitapp.models.QueryResultItem;
+
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ResultsPageFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ResultsPageFragment extends Fragment  implements ResultsRecyclerViewAdapter.customItemCLickListener   {
+
+    private static final String ARG_PARAM1 = "PagesList";
+    private static final String ARG_PARAM2 = "QueryKeys";
+
+    private ResultsRecyclerViewAdapter ResultsAdapter;
+    private RecyclerView ResultRecyclerView;
+    private ArrayList<QueryResultItem> ResultsList;//list of 10 or less result to display.
+    private ArrayList<String>QueryKeys;
+
+
+    public ResultsPageFragment() {
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param pagesList Parameter 1.
+     * @return A new instance of fragment com.example.ResultsPageFragment.
+     */
+    public static ResultsPageFragment newInstance(ArrayList<QueryResultItem> pagesList,ArrayList<String>QueryKeys) {
+        ResultsPageFragment fragment = new ResultsPageFragment();
+        Bundle args = new Bundle();
+        args.putParcelableArrayList(ARG_PARAM1,  pagesList);
+        args.putStringArrayList(ARG_PARAM2,  QueryKeys);
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            ResultsList = getArguments().getParcelableArrayList(ARG_PARAM1);
+            QueryKeys = getArguments().getStringArrayList(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_results_page, container, false);
+        ResultRecyclerView = (RecyclerView) view.findViewById(R.id.Results_recyclerView);
+        ResultRecyclerView.setLayoutManager(new LinearLayoutManager(getContext().getApplicationContext()));
+        ResultsAdapter = new ResultsRecyclerViewAdapter(ResultsList,QueryKeys,getContext().getApplicationContext(),this);
+        ResultRecyclerView.setAdapter(ResultsAdapter);
+        return view;
+    }
+
+
+    @Override
+    public void onItemClick(View v, int position) {
+
+    }
+}
