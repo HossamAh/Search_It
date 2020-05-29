ff46cb45706e235771e1cf9f6b88f5794a3a1035
Omar <somar3296@gmail.com>
2020-05-29 23:08:56 +0200

    image search in main

--- SearchItAPP/app/src/main/java/com/example/searchitapp/MainActivity.java
+++ SearchItAPP/app/src/main/java/com/example/searchitapp/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.provider.SearchRecentSuggestions;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.SearchView;
 import android.widget.Toast;
 
@@ -30,6 +31,7 @@ import java.util.stream.Stream;
 public class MainActivity extends AppCompatActivity {
     SearchRecentSuggestions suggestions = new SearchRecentSuggestions(this,
             MySuggestionProvider.AUTHORITY, MySuggestionProvider.MODE);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     private void processMyQuery(String query) {
+        CheckBox imageSearch=findViewById(R.id.checkBox);
     // query processing
     /*Toast.makeText(com.example.MainActivity.this,
     "you typed: "+query, Toast.LENGTH_LONG).show();*/
@@ -79,16 +82,16 @@ public class MainActivity extends AppCompatActivity {
                 "get","gets","getting","given","gives","go","goes","going","gone","got","gotten","greetings",
                 "had","hadn’t","half","happens","hardly","has","hasn’t","have","haven’t","having","he","he’d","he’ll","hello","help","hence","her","here","hereafter","hereby","herein","here’s","hereupon","hers","herself","he’s","hi","him","himself","his","hither","hopefully","how","howbeit","however","hundred",
                 "i’d","ie","if","ignored","i’ll","i’m","immediate","in","inasmuch","inc","inc.","indeed","indicate","indicated","indicates","inner","inside","insofar","instead","into","inward","is","isn’t","it","it’d","it’ll","its","it’s","itself","i’ve",
-                "just","keep","keeps","kept","know","known","knows","last","lately","later","latter","latterly","least","less","lest","let","let’s","like","liked","likely","likewise","little","look","looking","looks","low","lower","ltd",
+                "just","keep","keeps","kept","last","lately","later","latter","latterly","least","less","lest","let","let’s","like","liked","likely","likewise","little","look","looking","looks","low","lower","ltd",
                 "made","mainly","make","makes","many","may","maybe","mayn’t","me","mean","meantime","meanwhile","merely","might","mightn’t","mine","minus","miss","more","moreover","most","mostly","mr","mrs","much","must","mustn’t","my","mysel",
                 "name","namely","nd","near","nearly","necessary","need","needn’t","needs","neither","never","neverf","neverless","nevertheless","new","next","nine","ninety","no","nobody","non","none","nonetheless","noone","no-one","nor","normally","not","nothing","notwithstanding","novel","now","nowhere",
                 "obviously","of","off","often","oh","ok","okay","old","on","once","one","ones","one’s","only","onto","opposite","or","other","others","otherwise","ought","oughtn’t","our","ours","ourselves","out","outside","over","overall","own",
                 "particular","particularly","past","per","perhaps","placed","please","plus","possible","presumably","probably","provided","provides",
                 "rather","rd","re","really","reasonably","recent","recently","regarding","regardless","regards","relatively","respectively","right","round",
-                "said","same","saw","say","saying","says","second","secondly","see","seeing","seem","seemed","seeming","seems","seen","self","selves","sensible","sent","serious","seriously","seven","several","shall","shan’t","she","she’d","she’ll","she’s","should","shouldn’t","since","six","so","some","somebody","someday","somehow","someone","something","sometime","sometimes","somewhat","somewhere","soon","sorry","specified","specify","specifying","still","sub","such","sup","sure",
+                "said","same","saw","say","saying","says","second","secondly","see","seeing","seem","seemed","seeming","seems","seen","self","selves","sensible","sent","serious","seriously","seven","several","shall","shan’t","she","she’d","she’ll","she’s","should","shouldn’t","since","six","so","some","someday","somehow","someone","something","sometime","sometimes","somewhat","somewhere","soon","sorry","specified","specify","specifying","still","sub","such","sup","sure",
                 "take","taken","taking","tell","tends","th","than","thank","thanks","thanx","that","that’ll","thats","that’s","that’ve","the","their","theirs","them","themselves","then","thence","there","thereafter","thereby","there’d","therefore","therein","there’ll","there’re","theres","there’s","thereupon","there’ve","these","they","they’d","they’ll","they’re","they’ve","thing","things","think","third","thirty","this","thorough","thoroughly","those","though","three","through",
                 "throughout","thru","thus","till","to","together","too","took","toward","towards","tried","tries","truly","try","trying","t’s","twice","two",
-                "un","under","underneath","undoing","unfortunately","unless","unlike","unlikely","until","unto","up","upon","upwards","us","use","used","useful","uses","using","usually",
+                "un","under","underneath","undoing","unfortunately","unless","unlike","unlikely","until","unto","up","upon","upwards","useful","uses","using","usually",
                 "value","various","versus","very","via","viz","vs",
                 "want","wants","was","wasn’t","way","we","we’d","welcome","well","we’ll","went","were","we’re","weren’t","we’ve","what","whatever","what’ll","what’s","what’ve","when","whence","whenever","where","whereafter","whereas","whereby","wherein","where’s","whereupon","wherever","whether","which","whichever","while","whilst","whither","who","who’d","whoever","whole","who’ll","whom","whomever","who’s","whose","why","will","willing","wish","with","within","without","wonder","won’t","would","wouldn’",
                 "yes","yet","you","you’d","you’ll","your","you’re","yours","yourself","yourselves","you’ve",
@@ -118,6 +121,11 @@ public class MainActivity extends AppCompatActivity {
         final  String IP="192.168.1.3";
         final  int port=7800;
         String finalPhrase = phrase;
+        String imgSearch="@";
+        if(imageSearch.isChecked())
+            imgSearch+="yes";
+        else imgSearch+="non";
+        String finalImgSearch = imgSearch;
         Thread querySender =
         new Thread(
                 () -> {
@@ -125,8 +133,8 @@ public class MainActivity extends AppCompatActivity {
                     Socket S1 = new Socket(String.valueOf(IP), port);
                     DataOutputStream DOS = new DataOutputStream(S1.getOutputStream());
                       if(!finalPhrase.isEmpty())
-                          DOS.writeUTF(stemmed_query+ finalPhrase);
-                      else DOS.writeUTF(stemmed_query);
+                          DOS.writeUTF(stemmed_query+ finalPhrase+ finalImgSearch);
+                      else DOS.writeUTF(stemmed_query+finalImgSearch);
                    // DOS.writeUTF(stemmed_query);
                     DOS.flush();
                     DOS.close();
@@ -157,7 +165,6 @@ class Stemmer {
         StringBuilder stringBuilder = new StringBuilder();
         for (String word : replaceIllegalCharacter(s).split(" "))
         {
-            //  word = RemvoeStopWords(word);
             String stemmedWord = stemPrivate(word);
             if (StringUtils.isNotEmpty(stemmedWord)) {
                 if (stringBuilder.length() > 0)
