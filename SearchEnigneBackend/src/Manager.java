b3d15ef24f80c0b396bb8e837dc11c2989955728
HossamAh <hossamahmed201515@gmail.com>
2020-06-04 16:07:19 +0200

    manager modification to run pop function

--- SearchEnigneBackend/src/Manager.java
+++ SearchEnigneBackend/src/Manager.java
@@ -6,7 +6,8 @@ import java.util.Iterator;
 public class Manager {
     //public static Page.manageDB DB;
     private static HashSet<Crawler.OutputDoc> crawlerOutput;
-
+    private static long processedCrawledPages;
+    private static HashSet<Crawler.OutputDoc> outputSection;
     public static void main(String[] arg) throws IOException, URISyntaxException, InterruptedException {
         //crawlerOutput = new HashSet<>();
         //DB = new Page.manageDB();
@@ -29,6 +30,8 @@ public class Manager {
         new Thread(new Runnable() {
             @Override
             public void run() {
+                processedCrawledPages=0;
+                outputSection = new HashSet<>();
                 while (true) {
                     crawlerOutput = crawler.crawlerOutput;
                     if (crawlerOutput != null)
@@ -41,7 +44,23 @@ public class Manager {
                                 Crawler.OutputDoc output;
                                 synchronized (crawlerOutput) {
                                     output = itr.next();
+                                    outputSection.add(output);
                                 }
+                                if(processedCrawledPages%10==0)
+                                {
+                                    processedCrawledPages=0;
+                                    try {
+                                          Thread popThread=new Thread(new POPClass(outputSection));
+                                          popThread.start();
+                                          popThread.join();
+
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                    outputSection.clear();
+                                }
+
+                                processedCrawledPages +=1;
                                 //DB.docProcess(output);
                                 synchronized (crawlerOutput) {
                                     System.out.println(output.url);
@@ -55,5 +74,31 @@ public class Manager {
             }
 
         }).start();
+    }
+    public static class POPClass implements Runnable
+    {
+        private HashSet<Crawler.OutputDoc>PagesSet;
+
+        public POPClass(HashSet<Crawler.OutputDoc> pagesSet) {
+            PagesSet = pagesSet;
+        }
+
+        public HashSet<Crawler.OutputDoc> getPagesSet() {
+            return PagesSet;
+        }
+
+        public void setPagesSet(HashSet<Crawler.OutputDoc> pagesSet) {
+            PagesSet = pagesSet;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("in side pop function");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
